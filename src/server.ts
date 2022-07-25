2b7315df49041e98a10473638350fceb02c4ea48
Vinicius Santana <santanavinicius097@gmail.com>
2022-07-25 15:23:37 -0300

    modified:   src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express, { Application } from "express";
 
 
-
-
 const app : Application = express(); 
 const port = 3333;
 
-app.use(express.json);
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 
 app.listen(port, () => {
     console.log('Running on port 3333');
