c915e401fac30d13ffd76a2876e861967af32aa1
Vinicius Santana <santanavinicius097@gmail.com>
2022-07-26 15:04:11 -0300

    new file:   .dockerignore 	renamed:    build/server.js -> build/app.js 	new file:   build/bin/x64/node.exe 	new file:   dockerfile 	renamed:    src/server.ts -> src/app.ts 	new file:   web.config

--- build/app.js
+++ build/app.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3333;
-app.use(express_1.default.json);
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 app.listen(port, () => {
     console.log('Running on port 3333');
 });
